2458052226642e8bc40f6f5a116fd25305ad9f34
Konstantin Litvinov <klitvinov@itsupportme.com>
2016-12-28 17:12:08 +0300

    Add Dependency Injection.

---
+++ DependencyInjection/SbSAdminLTEExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SbS\AdminLTEBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class SbSAdminLTEExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+    /**
+     * Allow an extension to prepend the extension configurations.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $bundles = $container->getParameter('kernel.bundles');
+
+        if (isset($bundles['TwigBundle'])) {
+            $container->prependExtensionConfig(
+                'twig', [
+                    'form' => [
+                        'resources' => ['bootstrap_3_layout.html.twig']
+                    ]
+                ]
+            );
+        }
+
+        if (isset($bundles['AsseticBundle'])) {
+            $container->prependExtensionConfig(
+                'assetic', [
+                    'bundles' => ['SbSAdminLTEBundle']
+                ]
+            );
+
+        }
+    }
+}
